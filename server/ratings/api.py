1d5f34267e4dbeb6d4cee3d06a57f56820de4327
zeleza <mail@zeleza.ru>
2023-07-02 20:37:45 +0300

    Доработан серврер рейтинга, который из-за сбоя был стерт на предыдущем шаге.

--- server/ratings/api.py
+++ server/ratings/api.py
@@ -12,7 +12,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from telegram import Bot
-import os
+import os, math
 
 BOT_TOKEN = "6214126365:AAEVpcdojvmA47fF8afczPSI_zgJ1ueZTJ0"
 CHAT_ID = "-878944874"
@@ -21,9 +21,10 @@ bot = Bot(BOT_TOKEN)
 
 stage = "test"
 app = Flask(__name__)
-app_path = '/apps/samovar'
 rating_html_template = 'ratings.html'
-db_name = app_path + '/ratings_' + stage + '.db'
+api_path="/apps/api"
+root_request = "/api/server"
+db_name = api_path + '/ratings_' + stage + '.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -54,11 +55,11 @@ if not os.path.exists(db_name):
 
 @app.route('/test', methods=['GET'])
 def hello_world():
-    return "Hello World!"
+    return "Hello World!\n"
 
 # Маршрут для добавления новой оценки и отзыва пользователя
-@app.route('/rating', methods=['POST'])
-def add_rating():
+@app.route(root_request + '/send/review', methods=['POST'])
+async def add_rating():
     data = request.get_json()
     app_name = data.get('app_name')
     name = data.get('name')
@@ -70,13 +71,13 @@ def add_rating():
         new_rating = Rating(app_name, name, email, review, rating, version)
         db.session.add(new_rating)
         db.session.commit()
-        bot.send_message(chat_id=CHAT_ID, text=f'Новый отзыв на {app_name}:\nИмя: {name}\nEmail: {email}\nОтзыв: {review}\nРейтинг: {rating}\nВерсия: {version}')
+        await bot.send_message(chat_id=CHAT_ID, text=f'Новый отзыв на {app_name}:\nИмя: {name}\nEmail: {email}\nОтзыв: {review}\nРейтинг: {rating}\nВерсия: {version}')
         return jsonify({'success': True})
     else:
         return jsonify({'success': False})
 
 # Маршрут для просмотра списка всех приложений и их среднего рейтинга в браузере
-@app.route('/rating', methods=['GET'])
+@app.route(root_request + '/show/ratings', methods=['GET'])
 def get_rating():
     key = request.args.get('key')
     if key == 'secret_key':
@@ -88,29 +89,41 @@ def get_rating():
             apps[r.app_name].append(r.rating)
         avg_ratings = {}
         for app_name, app_ratings in apps.items():
-            avg_ratings[app_name] = sum(app_ratings) / len(app_ratings)
+            avg_ratings[app_name] = math.ceil(sum(app_ratings) / len(app_ratings))
+
+
         return render_template(rating_html_template, ratings=avg_ratings)
     else:
         return jsonify({'success': False})
 
 # Маршрут для получения подсчитанного рейтинга для конкретного приложения
-@app.route('/avg_rating', methods=['POST'])
-def avg_rating():
+@app.route(root_request + '/statistic', methods=['POST'])
+async def avg_rating():
     data = request.get_json()
     app_name = data.get('app_name')
-    ratings = Rating.query.filter_by(app_name=app_name).all()
+    app_version = data.get('version')
+    # проверяем передан ли номер версии программы
+    if not app_version or app_version == 'latest':
+        # если не передан, то находим крайнюю из тех что есть
+        app_version = db.session.query(db.func.max(Rating.version)).scalar()
+
+    ratings = Rating.query.filter_by(app_name=app_name, version=app_version).all()
+
     if ratings:
         voted = len(ratings)
-        avg_rating = sum([r.rating for r in ratings]) / voted
+        avg_rating = math.ceil(sum([r.rating for r in ratings]) / voted)
+        await bot.send_message(chat_id=CHAT_ID, text=f'Статистика {app_name}:\nСредняя оценка: {avg_rating}\nЧисло голосов: {voted}')
         return jsonify({
             'rating': avg_rating,
             'voted': voted,
+            'version': app_version,
         })
     else:
-        return jsonify({'rating': None})
+        return jsonify({'rating': None, 'voted': None, 'version': None})
+
 
 # Маршрут для получения списка всех отзывов для конкретного приложения
-@app.route('/reviews', methods=['POST'])
+@app.route(root_request + '/show/reviews', methods=['POST'])
 def get_reviews():
     data = request.get_json()
     app_name = data.get('app_name')
@@ -128,4 +141,4 @@ def get_reviews():
     return jsonify({'reviews': reviews})
 
 if __name__ == '__main__':
-    app.run(port=8166)
+    app.run(host='0.0.0.0', port=51153)
